6ba5cf0c4ce06d53fa11f6ff035681057fb018fe
Astraxx04 <gagan200254@gmail.com>
2023-10-06 15:10:03 +0530

    Mysql connection possible

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
 require("express-async-errors");
-const mysql = require("mysql");
 require("dotenv").config();
-const connectDB = require("./database/connect");
+const { Sequelize } = require('sequelize');
+const config = require('./database/config');
+
+
+
 
 
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-const host = process.env.DB_HOST
-const dbport = process.env.DB_PORT
-const user = process.env.DB_USER
-const password = process.env.DB_PASSWORD
+// const host = "database-1.cjgs35cvbfrq.us-east-1.rds.amazonaws.com";
+// const dbport = "5432";
+// const user = "astraxx04";
+// const password = "Astraxx2542";
+// const dataBase = "testing";
+
+// const sequelize = new Sequelize('testing', user, password, {
+//     host: host,
+//     dialect: 'postgres',
+//     port: dbport
+// });
+
+const sequelize = new Sequelize(config.development);
 
 const startServer = async() => {
     try {
-        await connectDB(host, dbport, user, password);
-        // console.log("RDS connection established successfully!");
+        console.log('Starting server');
+        await sequelize.authenticate();
         app.listen(port, console.log(`Server started on port ${port}...`));
     }
     catch (err) {
---
+++ database/config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    development: {
+      username: 'astraxx04',
+      password: 'Astraxx2542',
+      database: 'testingdb',
+      host: 'testing.cjgs35cvbfrq.us-east-1.rds.amazonaws.com',
+      port: '3306',
+      dialect: 'mysql',
+    },
+    production: {
+      // Production database configuration (if needed)
+    },
+};
+  
